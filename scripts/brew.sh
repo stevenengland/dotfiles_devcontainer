7dca9e5c8b4bcddf0621c206e430163eb6657adf
sten <github@steven-england.info>
2026-05-06 13:47:40 +0200

    fix: reorder brew.sh to improve Homebrew path setup and ensure proper environment initialization

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 set -e
-
 # Assumes passwordless sudo vscode user (devcontainer default)
 
 echo "Installing Homebrew..."
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Bootstrap brew onto PATH for the rest of this script
-echo >> "$HOME/.bashrc"
+# Detect prefix
 BREW_PREFIX=/home/linuxbrew/.linuxbrew
 [ -d /opt/homebrew ] && BREW_PREFIX=/opt/homebrew
+
+# Persist to .bashrc for future shells/scripts
+echo >> "$HOME/.bashrc"
+echo "eval \"\$(${BREW_PREFIX}/bin/brew shellenv bash)\"" >> "$HOME/.bashrc"
+
+# Bootstrap into current shell
 eval "$($BREW_PREFIX/bin/brew shellenv bash)"
 
 echo "Installing packages..."
